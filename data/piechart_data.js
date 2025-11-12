587983e78ee4437b0d940fe2462e18a727c552d6
John Huang <50758918+jier38@users.noreply.github.com>
2025-11-12 09:30:21 +0800

    Update piechart_data.js

--- data/piechart_data.js
+++ data/piechart_data.js
@@ -2,5 +2,4 @@ const pieChartData = [
   { deviceModel: "WIN-1500XE", count: 10 },
   { deviceModel: "OKI-RG7", count: 25 },
   { deviceModel: "HITACHI-CZ5000", count: 15 },
-  { deviceModel: "OTHER-MODEL", count: 5 },
 ];
